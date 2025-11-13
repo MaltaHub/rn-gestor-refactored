b435a9d608851336fbae748142f573fd677f772c
Kaic Malta <51099093+MaltaHub@users.noreply.github.com>
2025-11-13 18:24:37 +0000

    Nova versão

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  i18n: {
-    locales: ["en", "pt", "es"], // Idiomas suportados
-    defaultLocale: "pt", // Idioma padrão
-  },
 };
 
 export default nextConfig;
