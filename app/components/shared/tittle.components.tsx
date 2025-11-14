36e903e4e03e4e7525875ce271342ed27fec2495
Kaic Malta <51099093+MaltaHub@users.noreply.github.com>
2025-11-14 15:01:10 +0000

    Primeira Tabela

---
+++ app/components/shared/tittle.components.tsx
@@ -0,0 +1,7 @@
+export default function Tittle() {
+  return (
+    <h1 className="text-5xl font-extrabold text-zinc-900 dark:text-white sm:text-6xl">
+      Welcome to <span className="text-blue-600">rn-gestor!</span>
+    </h1>
+  );
+}
